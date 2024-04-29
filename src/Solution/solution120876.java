88e5f5dc6225ac8cf591fa76dfbb029158855c45
Huroad <cabin9999@naver.com>
2024-04-29 16:57:19 +0900

    겹치는 선분의 길이

--- src/Solution/solution120876.java
+++ src/Solution/solution120876.java
@@ -1,52 +1,46 @@
 package Solution120876;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class solution120876 {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int[][] lines = {{0, 1}, {2, 5}, {3, 9}};
+        int[][] lines = {{0, 5}, {0, 5}, {0, 5}};
         sol.solution(lines);
     }
 
     static class Solution {
         public int solution(int[][] lines) {
             int answer = 0;
-            int a1 = lines[0][0];
-            int a2 = lines[0][1];
-            int b1 = lines[1][0];
-            int b2 = lines[1][1];
-            int c1 = lines[2][0];
-            int c2 = lines[2][1];
-
-            int[] aArr = arr(a1, a2);
-            int[] bArr = arr(b1, b2);
-            int[] cArr = arr(c1, c2);
-            System.out.println(Arrays.toString(aArr));
-            System.out.println(Arrays.toString(bArr));
-            System.out.println(Arrays.toString(cArr));
-
-
-
-            return answer;
-        }
-        public int[] arr(int x, int y) {
-            int[] Arr;
-            if(x < 0) {
-                if(y < 0) {
-                    Arr = new int[Math.abs(Math.abs(x) - Math.abs(y)) + 1];
-                }else {
-                    Arr = new int[Math.abs(Math.abs(x) + Math.abs(y)) + 1];
+           List<Integer> arrlist = new ArrayList<>();
+            for (int i = 0; i < lines.length; i++) {
+                for (int j = lines[i][0]; j < lines[i][1]; j++) {
+                    arrlist.add(j + 1);
                 }
-            } else {
-                Arr = new int[Math.abs(Math.abs(x) - Math.abs(y)) + 1];
             }
-            for (int i = 0; i < Arr.length; i++) {
-                Arr[i] += x + i;
+            Collections.sort(arrlist);
+            System.out.println(arrlist);
+
+            for (int i = 0; i < arrlist.size(); i++) {
+                int cnt = 0;
+                // 여기서 j = i로 설정하지 않으면 이미 비교한값을 다시한번 비교함
+                for (int j = i; j < arrlist.size(); j++) {
+                    if(arrlist.get(i) == arrlist.get(j) && i != j) {
+                        arrlist.remove(j);
+                        cnt += 1;
+                    }
+                }
+                System.out.println(cnt);
+                if(cnt > 0) {
+                    answer += 1;
+                }
             }
-            return Arr;
+            System.out.println(arrlist);
+            System.out.println(answer);
+            return answer;
         }
     }
 }
-
