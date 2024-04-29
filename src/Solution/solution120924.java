1dd09ccd0667010c25e1305714df87cda6889f21
Huroad <cabin9999@naver.com>
2024-04-29 16:55:37 +0900

    다음에 올 숫자

---
+++ src/Solution/solution120924.java
@@ -0,0 +1,19 @@
+package Solution120956.src.Solution;
+
+public class solution120924 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] common = {1, 2, 3, 4};
+
+        sol.solution(common);
+    }
+    //다음에 올 숫자
+    static class Solution {
+        public int solution(int[] common) {
+            int answer = 0;
+
+
+            return answer;
+        }
+    }
+}
