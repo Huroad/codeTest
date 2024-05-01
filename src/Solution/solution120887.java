09532a1b58b2b969e52e730c981140081862d078
Huroad <cabin9999@naver.com>
2024-05-01 23:29:39 +0900

    k의 개수

---
+++ src/Solution/solution120887.java
@@ -0,0 +1,32 @@
+package Solution120876;
+
+public class solution120887 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int i = 10;
+        int j = 50;
+        int k = 5;
+
+        sol.solution(i, j ,k);
+    }
+    // k의 개수
+    static class Solution {
+        public int solution(int i, int j, int k) {
+            int answer = 0;
+            // i 부터 j까지 k가 몇번 등장하는지
+            String str = "";
+            for(int a = i; a<= j; a++) {
+                str += a;
+            }
+
+            for(int a = 0; a< str.length(); a++) {
+                if(k == Integer.parseInt(String.valueOf(str.charAt(a)))) {
+                    answer++;
+                }
+            }
+            System.out.println(answer);
+
+            return answer;
+        }
+    }
+}
